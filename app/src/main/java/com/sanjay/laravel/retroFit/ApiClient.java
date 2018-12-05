2cfcea74a9a856004b4c80d380a4455705067e86
sanjay kranthi kumar <kranthi.kumar@mahiti.org>
2018-12-05 10:11:56 +0530

    ADDED HTTPLOGGINGINTERCEPTOR

--- app/src/main/java/com/sanjay/laravel/retroFit/ApiClient.java
+++ app/src/main/java/com/sanjay/laravel/retroFit/ApiClient.java
@@ -1,5 +1,7 @@
 package com.sanjay.laravel.retroFit;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -8,11 +10,17 @@ import static com.sanjay.laravel.AppConstants.BASE_URL;
 
 public class ApiClient {
     private static Retrofit retrofit = null;
+    static OkHttpClient.Builder client = new OkHttpClient.Builder();
 
     public static Retrofit getApiClient() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        client.addInterceptor(loggingInterceptor);
         if (retrofit == null) {
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(client.build())
                     .addConverterFactory(GsonConverterFactory.create())
 //                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
